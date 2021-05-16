19ee9864ba6c95953cf649c94dde3764622c5ade
cernest <47100984+c-ernest@users.noreply.github.com>
2021-05-16 16:38:34 +0100

    Add files via upload

---
+++ If_statement.py
@@ -0,0 +1,10 @@
+def threeParameters(a, b, c):
+    new_c = int(c)
+    if a == b or a == new_c or b == new_c:
+        print(True)
+    else:
+        print(False)
+
+threeParameters(6, 5, "5")
+
+
