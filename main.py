09d23f6275d94a80f465aa2fe7478f8ef44932bf
cernest <47100984+c-ernest@users.noreply.github.com>
2021-05-16 16:34:02 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+#The MetaData of my Favourite Song
+
+#Initialize variables
+
+Album = "Dangerous"
+Artist = "Micheal Jackon"
+Genre = "Pop"
+Tempo = "80 beats per minute"
+WrittenKey = "A major"
+Duration = 376.2
+Release_Yr = 1991
+Producer = "Micheal Jackson and Bruce Swedien"
+
+# Print variables
+
+print (Album)
+print (Artist)
+print (Genre)
+print (Tempo)
+print (WrittenKey)
+print (Duration)
+print (Release_Yr)
+print (Producer)
+
