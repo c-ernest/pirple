a3a2f3d0dac94b4cf2b22653242f969fbc79390d
cernest <47100984+c-ernest@users.noreply.github.com>
2021-05-28 17:10:35 +0100

    Add files via upload

---
+++ Dict_and_Set.py
@@ -0,0 +1,9 @@
+FavSongDict = {"Album":"Dangerous","Artist":"Micheal Jackon","Genre":"Pop",
+               "Tempo":"80 beats per minute","WrittenKey":"A major",
+               "Duration":376.2,"Release_Yr":1991,
+               "Producer":"Micheal Jackson and Bruce Swedien"}
+
+
+for x,y in FavSongDict.items():
+    print(x,":",y)
+
